6a806d0b8d718aa7f35481f6c9378cc396d59eb8
Farel Lukas <farellukas@gmail.com>
2022-07-30 07:50:55 -0600

    Updated main pygame logic

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import pygame
+import pygame  # docs found here: https://www.pygame.org/docs/
+from sys import exit
 
 # initialize the pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800, 600))  # args: tuple(width, height)
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # customize the pygame window
 pygame.display.set_caption('calm the food down')
 # window_icon = pygame.image.load('')
 # pygame.display.set_icon(window_icon)
 
+# create Clock object
+clock = pygame.time.Clock()
+
 # game loop
-game_playing = True
-while game_playing:
+while True:
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # listen for QUIT event
-            game_playing = False
+            pygame.quit()
+            exit()
+
+    # updates display surface
+    pygame.display.update()
+    clock.tick(60)  # set fps ceiling to 60
